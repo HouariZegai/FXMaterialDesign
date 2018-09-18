487a54959b1379a3217d4b18832671a88d6bdf4f
zerrouki djamel <djameljimmizerrouki@gmail.com>
2018-09-18 20:42:55 +0200

    Adding selectedView

--- src/com/houarizegai/javafxmaterialdesign/java/controllers/MainController.java
+++ src/com/houarizegai/javafxmaterialdesign/java/controllers/MainController.java
@@ -4,8 +4,10 @@ import com.jfoenix.controls.JFXDialog;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,23 +18,33 @@ public class MainController implements Initializable {
     StackPane root;
     // Nodes
     private StackPane slideShowPane;
+    private VBox chackBoxpane;
 
     // Using to show nodes
     public static JFXDialog slideShowDialog;
+    public static JFXDialog  chackBoxDialog;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
             slideShowPane = FXMLLoader.load(getClass().getResource("/com/houarizegai/javafxmaterialdesign/resources/views/SlideShow.fxml"));
+            chackBoxpane = FXMLLoader.load(getClass().getResource("/com/houarizegai/javafxmaterialdesign/resources/views/chechBoxview.fxml"));
+
+
         } catch(IOException ioe) {
             ioe.printStackTrace();
         }
         slideShowDialog = new JFXDialog(root, slideShowPane, JFXDialog.DialogTransition.CENTER);
+        chackBoxDialog = new JFXDialog(root, chackBoxpane, JFXDialog.DialogTransition.CENTER);
 
     }
 
     @FXML
     private void onSlideShow() {
         slideShowDialog.show();
+    }
+
+    public void onrecyclerView(MouseEvent mouseEvent) {
+        chackBoxDialog.show();
     }
 }
